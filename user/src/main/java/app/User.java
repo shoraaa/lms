b9450796b05b8d0b3dc46e00ddc5d6fef522efac
HoangNgocNhi <23020130@vnu.edu.vn>
2024-10-04 22:28:40 +0700

    New update

--- user/src/main/java/app/User.java
+++ user/src/main/java/app/User.java
@@ -1,24 +1,27 @@
+package app;
+
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class User {
     protected String UserName;
     protected String RealName;
-    protected ArrayList<UserHistory> History;
+    protected ArrayList<UserHistory> History = new ArrayList<>();
 
     public User(String UN, String RN) {
         this.UserName = UN;
         this.RealName = RN;
     }
 
-    public void UpdateBorrowHistory(String DN, int num, Date br) {
-        UserHistory nH = new UserHistory(DN, num, br);
+    public void UpdateBorrowHistory(String DN, int num) {
+        UserHistory nH = new UserHistory(DN, num);
         History.add(nH);
     }
 
-    public void UpdateReturnHistory(String DN, Date re) {
+    public void UpdateReturnHistory(String DN) {
         for (UserHistory x : History) {
             if ((x.ReturnDay == null) && (x.DocName.equals(DN))) {
-                x.ReturnDay = re;
+                x.ReturnDay = Calendar.getInstance();
                 return;
             }
         }
@@ -40,5 +43,20 @@ public class User {
         for(UserHistory x: History) {
             x.print();
         }
+    }
+
+    public static void main(String[] args) {
+        User admin = new User("socnhi", "Ngoc Nhi");
+        admin.UpdateBorrowHistory("oop", 4);
+
+        admin.getDetail();
+    }
+
+    public ArrayList<UserHistory> getHistory() {
+        return History;
+    }
+
+    public void setHistory(ArrayList<UserHistory> History) {
+        this.History = History;
     }
 }
---
+++ user/src/main/java/app/UserHistory.java
@@ -0,0 +1,45 @@
+package app;
+import java.util.Calendar;
+
+public class UserHistory {
+    protected String DocName;
+    protected int quantity;
+    protected Calendar BorrowedDay;
+    protected Calendar ReturnDay;
+    protected Calendar Deadline;
+
+    public UserHistory() {
+        this.DocName = "";
+        this.quantity = 0;
+    }
+
+    public UserHistory(String DN, int num) {
+        this.DocName = DN;
+        this.quantity = num;
+        this.BorrowedDay = Calendar.getInstance();
+        this.Deadline = this.BorrowedDay;
+        this.Deadline.add(Calendar.DAY_OF_MONTH, 30);
+    }
+
+    public static void showCalendar(Calendar c) {
+        if (c == null) {
+            System.out.print("Books have not been returned!");
+            return;
+        }
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+ 
+        System.out.print(year + "-" + (month + 1) + "-" + day);
+    }
+
+    public void print() {
+        System.out.print(this.DocName + " " + this.quantity + " ");
+        showCalendar(BorrowedDay);
+        System.out.print(" ");
+        showCalendar(Deadline);
+        System.out.print(" ");
+        showCalendar(ReturnDay);
+        System.out.println(".");
+    }
+}

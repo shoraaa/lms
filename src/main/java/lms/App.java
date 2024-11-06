f66b78c939e67ebd63cf8c2fe6a0b25eef013aaa
shora <shoraaa@protonmail.com>
2024-11-06 17:23:28 +0700

    update App.java and App.class

--- src/main/java/lms/App.java
+++ src/main/java/lms/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("dashboard"), 640, 480);
+        scene = new Scene(loadFXML("dashboard"), 1920, 1080);
         stage.setScene(scene);
         stage.show();
     }

2c681cb0772cdd628d51000e7d76cc740ef2bf93
shora <shoraaa@protonmail.com>
2024-11-08 08:14:01 +0700

    update 8 files

--- src/main/java/com/library/model/DatabaseConnection.java
+++ src/main/java/com/library/model/DatabaseConnection.java
@@ -5,12 +5,35 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+/**
+ * Provides a connection to the library's database. The database is a SQLite
+ * database stored in a file named "library.db" in the current directory.
+ * <p>
+ * The database connection is a singleton, meaning that only one connection
+ * to the database is allowed to exist at any time. The first time the
+ * connection is requested, the database tables will be created if they do
+ * not already exist.
+ * <p>
+ * The database connection is automatically closed when the program exits.
+ *
+ * @author Dat
+ */
 public class DatabaseConnection {
     private static final String URL = "jdbc:sqlite:library.db";
     private static Connection connection;
 
     private DatabaseConnection() {}
 
+    /**
+     * Returns the database connection. If the connection does not already
+     * exist, it will be created. The database tables will also be created
+     * if they do not already exist.
+     * <p>
+     * If the connection is closed, it will be reopened.
+     *
+     * @return The database connection.
+     * @throws SQLException If the connection could not be created or opened.
+     */
     public static Connection getInstance() throws SQLException {
         if (connection == null || connection.isClosed()) {
             connection = DriverManager.getConnection(URL);
@@ -19,6 +42,12 @@ public class DatabaseConnection {
         return connection;
     }
 
+    /**
+     * Initializes the database tables. This method is called when the
+     * connection is first created.
+     *
+     * @param conn The database connection.
+     */
     private static void initializeDatabase(Connection conn) {
         String createAuthorsTable = "CREATE TABLE IF NOT EXISTS Authors (" +
                                     "author_id INTEGER PRIMARY KEY, " +
@@ -60,6 +89,10 @@ public class DatabaseConnection {
         }
     }
 
+    /**
+     * Closes the database connection. This method should be called when the
+     * connection is no longer needed.
+     */
     public static void closeConnection() {
         if (connection != null) {
             try {
--- src/main/java/com/library/model/Publisher.java
+++ src/main/java/com/library/model/Publisher.java
@@ -4,25 +4,49 @@ public class Publisher {
     private int id;
     private String name;
 
-    // Constructor
+    /**
+     * Constructor for Publisher.
+     * 
+     * @param id   The ID of the publisher.
+     * @param name The name of the publisher.
+     */
     public Publisher(int id, String name) {
         this.id = id;
         this.name = name;
     }
 
-    // Getters and Setters
+    /**
+     * Gets the ID of the publisher.
+     * 
+     * @return The publisher ID.
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Sets the ID of the publisher.
+     * 
+     * @param id The publisher ID.
+     */
     public void setId(int id) {
         this.id = id;
     }
 
+    /**
+     * Gets the name of the publisher.
+     * 
+     * @return The publisher name.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Sets the name of the publisher.
+     * 
+     * @param name The publisher name.
+     */
     public void setName(String name) {
         this.name = name;
     }
